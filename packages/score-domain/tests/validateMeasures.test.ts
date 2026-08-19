306fda671fffa1030630101fda5f5f20a25c16ad
yhh328 <yhyh328@naver.com>
2026-08-19 21:49:06 +0900

    fix: correct DEFAULT_PPQ import in measure tests

--- packages/score-domain/tests/validateMeasures.test.ts
+++ packages/score-domain/tests/validateMeasures.test.ts
@@ -3,7 +3,7 @@ import type { EntityId } from "../src/model/entityId";
 import type { Measure } from "../src/model/measure";
 import type { TimeSignature } from "../src/model/timeSignature";
 import { validateMeasures } from "../src/validation/validateMeasures";
-import { DEFAULT_PPQ } from "../src/validation/isValidPPQ";
+import { DEFAULT_PPQ } from "../src/model/score";
 
 const PPQ = DEFAULT_PPQ;
 const PATH = "measures";
